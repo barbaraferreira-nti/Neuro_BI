9d9170a741caa131bf5741bf83617f76d5a3859c
Barbara Ferreira <barbara@neurosaber.com.br>
2026-04-15 08:24:21 -0300

    retirado as partes de códigos que estavam comentadas - 150426

--- ProjetosPython/Pipelines/Sales/atualizaFactSales.py
+++ ProjetosPython/Pipelines/Sales/atualizaFactSales.py
@@ -6,11 +6,9 @@ import traceback
 import pandas as pd
 import math
 
-# dataI = (datetime.date.today() - datetime.timedelta(days=30)).strftime("%Y-%m-%d")
-# dataF = datetime.date.today().strftime("%Y-%m-%d")
+dataI = (datetime.date.today() - datetime.timedelta(days=30)).strftime("%Y-%m-%d")
+dataF = datetime.date.today().strftime("%Y-%m-%d")
 
-dataI = "2026-01-01"
-dataF = "2026-02-28"
 
 banco = "Guru_DB"
 tabela = "fact_sales"
@@ -98,37 +96,37 @@ print("Dados de vendas da Guru atualizados com sucesso!")
 
 
 ### 2. Atualizar as vendas da Principia
-# try:
-#     print(f"Buscando dados de vendas da Principia para o período de {dataI} a {dataF}...")
+try:
+    print(f"Buscando dados de vendas da Principia para o período de {dataI} a {dataF}...")
 
-#     df = metodosPrincipia.api.getSalesDF(app="PrincipiaApi", periodo=[dataI, dataF], ambiente="url_prod")
+    df = metodosPrincipia.api.getSalesDF(app="PrincipiaApi", periodo=[dataI, dataF], ambiente="url_prod")
 
-#     if df.empty:
-#         print("Aviso: nenhum dado retornado.")
-#     else:
-#         if "id" not in df.columns:
-#             raise ValueError("A coluna 'id' não existe no DataFrame.")
-#         if df["id"].isnull().any():
-#             raise ValueError("Existem registros com id nulo.")
+    if df.empty:
+        print("Aviso: nenhum dado retornado.")
+    else:
+        if "id" not in df.columns:
+            raise ValueError("A coluna 'id' não existe no DataFrame.")
+        if df["id"].isnull().any():
+            raise ValueError("Existem registros com id nulo.")
         
-#         rows = df.to_dict(orient="records")
-#         rows = normalizar_rows(rows)
-#         total_rows = len(rows)
-#         print(f"Total de registros para upsert: {total_rows}")
+        rows = df.to_dict(orient="records")
+        rows = normalizar_rows(rows)
+        total_rows = len(rows)
+        print(f"Total de registros para upsert: {total_rows}")
 
-#         for i in range(0, total_rows, batch_size):
-#             lote = rows[i:i + batch_size]
-#             try:
-#                 upsert = metodos_supabase.api.upsert_data(banco=banco, tabela=tabela, dados=rows, chave="id")
-#                 print(f"Upsert concluído com sucesso. Lote {i//batch_size + 1} | Registros {i+1} até {i+len(lote)}")
-#             except Exception as e:
-#                 print(f"Erro no lote {i//batch_size + 1} | Registros {i+1} até {i+len(lote)}")
-#                 print(str(e))
-#                 print(traceback.format_exc())
-# except Exception as e:
-#     print(f"Erro ao fazer upsert na tabela {tabela}' no banco '{banco}'.")
-#     print(str(e))
-#     print(traceback.format_exc())
+        for i in range(0, total_rows, batch_size):
+            lote = rows[i:i + batch_size]
+            try:
+                upsert = metodos_supabase.api.upsert_data(banco=banco, tabela=tabela, dados=rows, chave="id")
+                print(f"Upsert concluído com sucesso. Lote {i//batch_size + 1} | Registros {i+1} até {i+len(lote)}")
+            except Exception as e:
+                print(f"Erro no lote {i//batch_size + 1} | Registros {i+1} até {i+len(lote)}")
+                print(str(e))
+                print(traceback.format_exc())
+except Exception as e:
+    print(f"Erro ao fazer upsert na tabela {tabela}' no banco '{banco}'.")
+    print(str(e))
+    print(traceback.format_exc())
     
-# print("Dados de vendas da Principia atualizados com sucesso!")
+print("Dados de vendas da Principia atualizados com sucesso!")
 
